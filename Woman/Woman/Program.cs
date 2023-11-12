58effc7c8a4bf5e4f66e11d3c2870990802904c1
misiopysiochojnacki@gmail.com <misio@MACIUSIEK>
2023-11-12 23:57:50 +0100

    Zadanie 4 dzień

---
+++ Woman/Woman/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System.ComponentModel.Design;
+
+string name = "Ewa";
+int age = 25;
+string gender = "Woman";
+string gender2 = "Male";
+
+if (gender == "Woman" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33) ;
+{
+    Console.WriteLine("Ewa lat 33");
+}
+
+if (gender2 == "Male" && age < 18) ;
+{
+    Console.WriteLine("Niepełnoletni mezczyzna");
+}
