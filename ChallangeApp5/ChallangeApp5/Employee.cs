8b6f7e37ecea20d9d5435ff90448909c3ff68463
misiopysiochojnacki@gmail.com <misio@MACIUSIEK>
2023-12-16 13:09:25 +0100

    Zadanie 6

---
+++ ChallangeApp5/ChallangeApp5/Employee.cs
@@ -0,0 +1,32 @@
+﻿namespace ChallangeApp5;
+
+public class Employee
+{
+    public Employee(string name, string surname, int age)
+    {
+        this.Name = name;
+        this.Surname = surname;
+        this.Age = age;
+    }
+    public string Name { get; set; }
+
+    public string Surname { get; set; }
+
+    public int Age { get; set; }
+
+    private List<int> points = new List<int>();
+
+    public void AddPoints(int score)
+    {
+        this.points.Add(score);
+    }
+
+    public int Result
+    {
+        get
+        {
+            return this.points.Sum();
+        }
+    }
+}
+
